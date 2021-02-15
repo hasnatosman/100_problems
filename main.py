ace35d7c4ecbd7c8e3b6f4612db8a1364aeffcb4
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-02-15 12:20:28 +0600

    let's go.

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+It,s became a daily habit to practice python. May be not that hard or advanced just
+keep going with easier to next.
+ So I will try to solve around 100 plus problems. It will help me to keep in touch.
+  I really love to code in python. It gives me different.
+"""
